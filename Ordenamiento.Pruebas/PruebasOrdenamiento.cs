b17a82b031f35cf1e65d7810f4a9d018c3acb9e5
dgarciaap <dgarcia24@ucol.mx>
2019-03-21 10:16:56 -0600

    Agregado el proyecto de pruebas

---
+++ Ordenamiento.Pruebas/PruebasOrdenamiento.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tdd_dgarciaap
+{
+    [TestClass]
+    public class PruebasOrdenamiento
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
