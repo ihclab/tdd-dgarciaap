0bb19dbd37543fa73db631b0c9b5be7b757b694e
dgarciaap <dgarcia24@ucol.mx>
2019-03-25 20:54:26 -0600

    Arreglar la clase ordenamiento

--- Ordenamiento.Pruebas/PruebasOrdenamiento.cs
+++ Ordenamiento.Pruebas/PruebasOrdenamiento.cs
@@ -3,11 +3,69 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Ordenamiento.Pruebas
 {
     [TestClass]
-    public class UnitTest1
+    public class PruebasOrdenamiento
     {
+        private int[] salidaEsperada = new int[] { 1, 2, 3, 4, 5 };
+        private int[] salidaEsperada2 = new int[] { 1, 1, 1, 1, 1 };
+        private int[] salidaEsperada3 = new int[] { 1, 2, 3, 4, 4 };
+        private Ordenamiento.Burbuja ordenar = new Ordenamiento.Burbuja();
+
         [TestMethod]
-        public void TestMethod1()
+
+         public void TestInvertido()
         {
+            int[] entrada = new int[] { 5, 4, 3, 2, 1 };
+
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void TestOrdenado()
+        {
+            int[] entrada = new int[] { 1, 2, 3, 4, 5 };
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void TestChueco()
+        {
+            int[] entrada = new int[] { 1, 3, 2, 4, 5 };
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void TestElCincoPorDelante()
+        {
+            int[] entrada = new int[] { 5, 1, 2, 3, 4 };
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void TestNoSeComoPonerle()
+        {
+            int[] entrada = new int[] { 2, 3, 4, 5, 1 };
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void TestUno()
+        {
+            int[] entrada = new int[] { 1, 1, 1, 1, 1 };
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada2, entrada);
+        }
+
+        [TestMethod]
+        public void TestUltimo()
+        {
+            int[] entrada = new int[] { 1, 4, 3, 4, 2 };
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada3, entrada);
         }
     }
 }
--- ordenamiento/Burbuja.cs
+++ ordenamiento/Burbuja.cs
@@ -5,7 +5,18 @@ using System.Text;
 namespace Ordenamiento {
     public class Burbuja {
         public void Ordenar(int[] arr) {
-            throw new NotImplementedException("Crear los casos de prueba");
+             for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = arr.Length - 1; j >= i; j--)
+                {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        int aux = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = aux;
+                    }
+                }
+            }
         }
     }
 }
